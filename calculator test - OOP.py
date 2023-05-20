e0281c51d9fd51b5e1a3c2bbebfa00ed8e7c8bc4
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-05-20 23:37:28 +0800

    math operations

--- calculator test - OOP.py
+++ calculator test - OOP.py
@@ -1,7 +1,15 @@
 # Franchesca Marie U. Donadillo
 # BSCPE 1-5
 
+# import user interface
+from user_interface import UserInterface
+
+u_interface = UserInterface()
+
+# import Calculator
 # Ask user for mathematical operations to be used
+u_interface.math_operation()
+
 # Ask user for two numbers
 # Perform calculations
 # Display result
--- user_interface.py
+++ user_interface.py
@@ -1,10 +1,28 @@
 # Franchesca Marie U. Donadillo
 # BSCPE 1-5
 
-# initialize
 # Create user interface class
-# Sefine function user_input
-# Display sum
-# Display difference
-# Display product
-# Display quotient
+class UserInterface:
+    # initialize
+    def __init__(self):
+        print("MATHEMATICAL OPERATIONS\n")
+        print("~ Addition")
+        print("~ Subtraction")
+        print("~ Multiplication")
+        print("~ Division\n")
+
+    # Get user's mathematical operation
+    def math_operation(self):
+        operation = input("Enter the mathematical operation you want to perform: ")
+        return operation
+    
+    # Define function user_input
+    def user_input(self):
+        num_1 = float(input("Enter first number: "))
+        num_2 = float(input("Enter second number: "))
+        return num_1,num_2
+
+    # Display sum
+    # Display difference
+    # Display product
+    # Display quotient
