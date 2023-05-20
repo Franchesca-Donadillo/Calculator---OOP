9330814e551054766315622955e0a3dd63ec0c1a
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-05-20 21:40:52 +0800

    initial pseudocode

---
+++ calculator test - OOP.py
@@ -0,0 +1,7 @@
+# Franchesca Marie U. Donadillo
+# BSCPE 1-5
+
+# Ask user for mathematical operations to be used
+# Ask user for two numbers
+# Perform calculations
+# Display result
---
+++ user_interface.py
@@ -0,0 +1,10 @@
+# Franchesca Marie U. Donadillo
+# BSCPE 1-5
+
+# initialize
+# Create user interface class
+# Sefine function user_input
+# Display sum
+# Display difference
+# Display product
+# Display quotient
