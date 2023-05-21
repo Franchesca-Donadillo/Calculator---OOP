35aa035c49d434613882a4856af303ddf7e01bc0
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-05-21 21:19:22 +0800

    Modified program

--- calculator test - OOP.py
+++ calculator test - OOP.py
@@ -23,38 +23,38 @@ def main():
         while True:
             try:
                 # Ask user for two numbers
-                num_1 = float(u_interface.user_input())
-                num_2 = float(u_interface.user_input())
+                num_1 = float(u_interface.user_input(" first "))
+                num_2 = float(u_interface.user_input(" second "))
 
                 # Perform calculations
                 if user_operation == "ADDITION":
                     sum = calc.add(num_1, num_2)
                     # Display resultof addition
-                    sum_display = u_interface.display_sum(sum)
+                    u_interface.display_sum(sum)
 
                 elif user_operation == "SUBTRACTION":
                     diff = calc.sub(num_1, num_2)
                     # Display result of subtraction
-                    display_diff = u_interface.display_diff(diff)
+                    u_interface.display_diff(diff)
 
                 elif user_operation == "MULTIPLICATION":
                     prod = calc.multiply(num_1, num_2)
                     # Display result of multiplication
-                    display_prod = u_interface.display_prod(prod)
+                    u_interface.display_prod(prod)
 
                 elif user_operation == "DIVISION":
                     quo = calc.div(num_1,num_2)
                     # Display result of division
-                    display_quo = u_interface.display_quo(quo)
+                    u_interface.display_quo(quo)
             
             except ValueError:
-                print("Invalid value. Enter numbers only.")
+                print("Invalid value. Enter numbers only.\n")
 
             except ZeroDivisionError:
-                print("Invalid. You are dividing by 0.")
+                print("Invalid. You are dividing by 0.\n")
 
             finally:
                 # ask user if they want to repeat
-                rep = u_interface.user_repeat(main)
+                u_interface.user_repeat(main)
 
 main()
--- user_interface.py
+++ user_interface.py
@@ -13,31 +13,36 @@ class UserInterface:
 
     # Get user's mathematical operation
     def math_operation(self):
-        operation = input("Enter the mathematical operation you want to perform: ").upper()
+        operation = input("Enter the mathematical operation you want to perform between two numbers: ").upper()
         return operation
     
     # Define function user_input
-    def user_input(self):
-        num = float(input("Enter number: "))
+    def user_input(self, num_name):
+        num = float(input("Enter" + num_name + "number: "))
         return num
 
     # Display sum
     def display_sum(self, sum):
-        print("The sum of the numbers is " + str(sum))
+        print("="*83)
+        print("The sum of the numbers is " + str(sum), "\n")
 
     # Display difference
     def display_diff(self,diff):
-        print("The difference of the numbers is " + str(diff))
+        print("="*83)
+        print("The difference of the numbers is " + str(diff), "\n")
         
     # Display product
     def display_prod(self, prod):
-        print("The product of the numbers is " + str(prod))
+        print("="*83)
+        print("The product of the numbers is " + str(prod), "\n")
 
     # Display quotient
     def display_quo(self, quo):
-        print("The quotient of the numbers is " + str(quo))
+        print("="*83)
+        print("The quotient of the numbers is " + str(quo), "\n")
 
     def user_repeat(self, main):
+        print("="*83)
         repeat = input("Do you want to repeat? yes/no: ")
         print("\n")
 
@@ -48,4 +53,17 @@ class UserInterface:
             print("THANK YOU!")
             exit()
 
-        return repeat
+        else:
+            print("Invalid. Type yes or no only.\n")
+            print("="*83)
+            repeat = input("Do you want to repeat? yes/no: ")
+            print("\n")
+
+            if repeat == "yes":
+                main()
+
+            elif repeat == "no":
+                print("THANK YOU!")
+                exit()
+
+    
